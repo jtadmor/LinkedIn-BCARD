32f52ef5b49188e3baf56da068dd79df080881b1
jtadmor <jtadmor@gmail.com>
2014-12-01 12:10:39 -0700

    hack fix?

--- Controllers/apiController.js
+++ Controllers/apiController.js
@@ -20,11 +20,17 @@ var apiController = {
 		// Grab the id, field that was edited, and the new value from req.body
 		// Find by id and update
 		// No need to send anything back because it is already re-rendered on the client
-		console.log('data sent:', req.body)
-		var profile = req.body;
-		delete profile._id;
-		console.log('profile to save:', profile);
-		Profile.findOneAndUpdate({_id: req.body._id}, {$set: profile}, function(err, doc) {
+		Profile.findOneAndUpdate({_id: req.body._id}, {$set: {
+			name: req.body.name,
+			emailAddress: req.body.emailAddress,
+			headline: req.body.headline,
+			location: req.body.location,
+			pictureURL: req.body.pictureURL,
+			company: req.body.company,
+			title: req.body.title,
+			skills: req.body.skills,
+			edit_pw: req.body.edit_pw
+		}}, function(err, doc) {
 			if (err)
 				console.log(err)
 			console.log('saved doc:', doc);
